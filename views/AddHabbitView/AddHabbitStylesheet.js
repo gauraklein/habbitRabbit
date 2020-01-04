f120c43a840cb08c3b539a097b3af5d47b83d250
gauraklein <gaurakklein@gmail.com>
2020-01-04 12:30:09 -0600

    Basic set up for login

--- views/AddHabbitView/AddHabbitStylesheet.js
+++ views/AddHabbitView/AddHabbitStylesheet.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
     container: {
         flex: 9,
-        backgroundColor: '#ddd',
+        backgroundColor: '#EAEAEA',
         alignItems: 'center',
         justifyContent: 'center',
         color: '#fff'
