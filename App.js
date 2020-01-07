5e426bf6da0204ac3b6f5cc8bb2cb895d6c70d2c
gauraklein <gaurakklein@gmail.com>
2020-01-07 12:13:06 -0600

    added in thunk for async

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {initStore} from './redux/store'
 import {Provider} from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './redux/reducers/rootReducer'
 import('./ReactotronConfig').then(() => console.log('Reactotron Configured'))
 import AppContainer from './AppContainer'
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
-const store = initStore();
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default function App() {
   return (
