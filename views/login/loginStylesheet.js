bbeee5dcfc78b434aaf0f3ae9b8db4b72e4ad006
gauraklein <gaurakklein@gmail.com>
2020-01-07 01:44:44 -0600

    nearing the end of styling

--- views/login/loginStylesheet.js
+++ views/login/loginStylesheet.js
@@ -3,8 +3,8 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
     container: {
         flex: 9,
-        backgroundColor: '#ddd',
-        alignItems: 'center',
+        // backgroundColor: '#ddd',
+        // alignItems: 'center',
         justifyContent: 'center',
         color: '#fff'
     },
@@ -15,7 +15,37 @@ export const styles = StyleSheet.create({
         flex: 1
     },
     text: {
-        color: 'black'
+        color: 'black',
+        padding: 5
+    },
+    customBtnText: {
+        fontSize: 20,
+        fontWeight: '400',
+        color: "black",
+        textAlign: "center",
+        // borderWidth: 3,
+        paddingHorizontal: 0
+    },
+    largeText: {
+        color: 'black',
+        fontSize: 35,
+        fontWeight: "bold",
+        textAlign: "center",
+        // flex: 1
+    },
+
+  /* Here, style the background of your button */
+    customBtnBG: {
+    marginTop: 20,
+    marginBottom: 20,
+    backgroundColor: "#edf5e1",
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+    borderRadius: 30,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
     }
 
 });
